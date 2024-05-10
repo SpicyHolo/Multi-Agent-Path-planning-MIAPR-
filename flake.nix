a1860dac57bfba771bb57f815a6f0718633a2180
SpicyHolo <41269364+SpicyHolo@users.noreply.github.com>
2024-05-10 23:32:37 +0200

    init for dev on nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A nix-flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+        supportedSystems = [ "x86_64-linux" ];
+        forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+            pkgs = import nixpkgs { inherit system; };
+        });
+    in
+    {
+        devShells = forEachSupportedSystem({ pkgs }: {
+            default = pkgs.mkShell.override {
+                stdenv = pkgs.gccStdenv;
+            }
+            {
+                packages = with pkgs; [
+                    cmake
+                    boost
+                    python311
+                ] ++
+                (with pkgs.python311Packages; [
+                    pybind11
+                ]);
+            };
+        });
+    };
+}
