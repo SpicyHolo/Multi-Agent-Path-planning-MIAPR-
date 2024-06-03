4205cf72e423ba5638f45ed0f390af72dc119ccf
SpicyHolo <41269364+SpicyHolo@users.noreply.github.com>
2024-06-03 13:38:50 +0200

    Add testing script

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+echo "Robot planning tester v 1.0 UwU"
+cat "./test/cat.txt"
+usleep 100000
+echo "Building project... (python)"
+sleep 1
+bash ./compile.sh
+echo "Compilation finished."
+sleep 1
+echo "Running small test case"
+sleep 1
+./build/lifelong --inputFile ./example_problems/random.domain/random_20.json \
+    --simulationTime 50 --logFile ./test/log.txt --output ./test/out.json
+echo "Produced: './test/out.json'"
+sleep 1
+echo "Processing file:"
+
